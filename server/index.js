a5a41683e876fa5a30f3951a9f89699566ec8f1d
Omar Mahdy <omar.mahdy246@gmail.com>
2023-04-06 11:43:44 +0800

    index.js code completed

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const CONNECTION_URL =
+  "mongodb+srv://root:st9ND7o6BalgHkQF@cluster0.hedabsx.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
